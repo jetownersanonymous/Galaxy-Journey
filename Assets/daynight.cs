cb5716521bd04f132ac3985ba2d933e7ac5c7394
Jake Shasteen <jake@jetownersanonymous.com>
2014-10-04 15:08:00 -0400

    Playing around with day/night cycler

---
+++ Assets/daynight.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class daynight : MonoBehaviour 
+{
+	public float speed = 1.0f;
+
+	// Update is called once per frame
+	void Update () 
+	{
+	
+		transform.Rotate (new Vector3 ( Time.deltaTime * speed, 0, 0));
+
+
+	}
+}
