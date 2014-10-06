80133fe57abe1e00541f7b2c071e287f5fa71b2e
Jake Shasteen <jake@jetownersanonymous.com>
2014-10-06 01:01:40 -0400

    Updated random map generator script

--- Assets/Scripts/GenerateMap.cs
+++ Assets/Scripts/GenerateMap.cs
@@ -7,7 +7,11 @@ public class GenerateMap : MonoBehaviour
 	public int seed = 42;
 	public int minRooms = 2;
 	public GameObject emptyRoom;
-	int currentRoom;
+	public float recursiveChance = .95f;
+	public int maxMakers = 3;
+	public float biasFrequency = .5f;
+	static int currentRoom;
+	static int numberOfMakers = 1;
 //	public GameObject cornerRoom;
 //	public GameObject wallRoom;
 //	public GameObject deadEnd;
@@ -21,7 +25,10 @@ public class GenerateMap : MonoBehaviour
 		Random.seed = seed;
 
 		// initialize location
+		if ( numberOfMakers == 1 )
+		{
 		transform.position = new Vector3( 0, 0, 0 );
+		}
 
 		// Min ## rooms is 2.
 		if ( minRooms < 2 ) { minRooms = 2; }
@@ -29,19 +36,26 @@ public class GenerateMap : MonoBehaviour
 
 	void Update ()
 	{
-		// define our variables!
+		makeRooms ( this.gameObject );
+	}
 
+	void makeRooms ( GameObject maker )
+	{
+		// define our variables!
 		// Figure out the sizes of our rooms! Assume all modules are the same width/length.
 		float roomWidth = emptyRoom.renderer.bounds.size.x;
 		float roomLength = emptyRoom.renderer.bounds.size.z;
 
+		// For the random direction, biased toward origin.
+		int directionChosen;
+		
 		//We need this raycast to find out if the space we want to place our new room is occupied or not.
 		RaycastHit hit;
 		Ray anyoneHome;
-
+		
 		// current object for renaming and moving around.
 		Object current;
-
+		
 		/* Example Room 
 		// Fill the corners
 		current = Instantiate( cornerRoom, new Vector3 ( roomWidth * width, 0 , 0), Quaternion.Euler ( new Vector3( 0, 270, 0 ) ) );
@@ -75,13 +89,13 @@ public class GenerateMap : MonoBehaviour
 		current = Instantiate( wallRoom, new Vector3 (width * roomWidth, 0, roomLength * z), Quaternion.Euler (new Vector3( 0, 180, 0)));
 		current.name = "rightwall";
 		} */
-
-
+		
+		
 		if ( currentRoom < minRooms )
 		{
 			// We're gonna look down to see if there's a room there.
 			anyoneHome = new Ray( new Vector3 ( transform.position.x , transform.position.y + 1, transform.position.z ), Vector3.down );
-
+			
 			// check if something is already here!
 			if ( ! Physics.Raycast ( anyoneHome, out hit ) ) 
 			{
@@ -90,39 +104,95 @@ public class GenerateMap : MonoBehaviour
 				current.name = "Room #"+currentRoom;
 				currentRoom += 1;
 			}
-
+			
 			// Where to next?
 
-			switch( Random.Range ( 0, 3 ) )
+			directionChosen = Random.Range (0, 3);
+
+			// let's bias toward the origin.
+			if (Random.value < biasFrequency)
+			{
+				if (Mathf.Abs ( maker.transform.position.x ) > Mathf.Abs ( maker.transform.position.z ) )
+				{
+					// we know the east-west magnitude is stronger than the north-south
+				if ( directionChosen == 1 || directionChosen == 3)
+				{
+					directionChosen += Random.Range (-1, -3);
+				//		directionChosen +=1;
+				//		directionChosen = 0;
+				}
+					
+				}
+				else
+				{
+					// the north-south magnitude is stronger than the east-west.
+				if ( directionChosen == 0 || directionChosen == 2)
+				{
+					directionChosen += Random.Range (-1, -3);
+				//		directionChosen +=1;
+				//		directionChosen = 1;
+				}
+
+				}
+
+			// fix any issues with being outside of the range
+			if(directionChosen > 3)
+			{ 
+				directionChosen -= 4; 
+			}
+			else if (directionChosen < 0)
+			{
+				directionChosen += 4;
+			}
+			if(directionChosen > 3)
+			{ 
+				directionChosen = 0; 
+			}
+			else if (directionChosen < 0)
+			{
+				directionChosen = 3;
+			}
+			}
+			// and move!
+			Debug.Log (directionChosen);
+			switch( directionChosen )
 			{
 			case 0:
 				// north
-				transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + roomLength);
-
+				maker.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + roomLength);
+				
 				break;
 			case 1:
 				// east
-				transform.position = new Vector3(transform.position.x + roomWidth, transform.position.y, transform.position.z);
-
+				maker.transform.position = new Vector3(transform.position.x + roomWidth, transform.position.y, transform.position.z);
+				
 				break;
 			case 2:
 				// south
-				transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - roomLength);
-
+				maker.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - roomLength);
+				
 				break;
 			case 3:
 				// west
-				transform.position = new Vector3(transform.position.x - roomWidth, transform.position.y, transform.position.z);
-
+				maker.transform.position = new Vector3(transform.position.x - roomWidth, transform.position.y, transform.position.z);
+				
 				break;
 			}
-		
-		if ( currentRoom == minRooms )
+			
+			if ( currentRoom == minRooms )
 			{
 				// Kill the generator.
-				Destroy(this);
+				Destroy( maker );
+				numberOfMakers -=1;
 			}
 
+			// add some recursive chaos
+			if ( Random.value > recursiveChance && numberOfMakers < maxMakers )
+			{
+				Instantiate ( maker, transform.position, Quaternion.identity);
+				numberOfMakers +=1;
+			}
+			
 		}
 	}
 
